d7988dd53e6a46edc80868749f5822e5b973c8fa
Luan Daros <darosluan@gmail.com>
2022-01-11 14:20:49 -0300

    rename

--- tests/unit/product.spec.js
+++ tests/unit/product.spec.js
@@ -6,23 +6,23 @@ const data = {
   name: "TESTNAME1",
   price: 69.69,
   weight: "1",
-  image: ".../",
+  image: "...",
   id: 101,
 };
 
-test("renders product component", () => {
+test("Renders product component", () => {
   // mount() returns a wrapped Vue component we can interact with
   const wrapper = mount(Product, { propsData: { product: data } });
 
   // Find the product name using class name
   const productName = wrapper.find(".product-name");
 
-  // Expect the product name to be the same as the data
-  expect(productName.text()).toBe("TESTNAME1");
+  // Expect the product name to be the same as in data
+  expect(productName.text()).toBe(data.name);
 
   // Find the product price using id
   const productPrice = wrapper.find("#prod-price");
 
-  // Expect the product price to be the same as the data
-  expect(productPrice.text()).toBe("$69.69");
+  // Expect the product price to be the same as in data
+  expect(productPrice.text()).toBe("$" + data.price.toString());
 });
